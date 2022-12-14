26e691cf0f40f148fdba00122b7ae2eb73295285
Dylan Davis <dylan.davis.uk@outlook.com>
2022-12-14 22:39:36 +0000

    Base structure.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Day: ");
+            var day = GetIntAsInput();
+
+            Console.Write("Input: ");
+            var input = GetIntAsInput();
+
+            Day1();
+        }
+
+        private static void Day1(int calories)
+        {
+            
+        }
+
+        private static int GetIntAsInput()
+        {
+            var result = 0;
+
+            string? input = Console.ReadLine();
+
+            while (!int.TryParse(input, out result))
+            {
+                input = Console.ReadLine();
+                Console.Clear();
+            }
+
+            return result;
+        }
+    }
+}
